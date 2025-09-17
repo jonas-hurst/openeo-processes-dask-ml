71070eb55dc48b09449c5d42105d0db1c380640f
= <jonhur@web.de>
2025-09-17 11:42:57 +0200

    removed dimension property

--- openeo_processes_dask_ml/process_implementations/ml_predict.py
+++ openeo_processes_dask_ml/process_implementations/ml_predict.py
@@ -3,8 +3,6 @@ import xarray as xr
 from .data_model import MLModel
 
 
-def ml_predict(
-    data: xr.DataArray, model: MLModel, dimension: list[str]
-) -> xr.DataArray:
+def ml_predict(data: xr.DataArray, model: MLModel) -> xr.DataArray:
     out = model.run_model(data)
     return out
