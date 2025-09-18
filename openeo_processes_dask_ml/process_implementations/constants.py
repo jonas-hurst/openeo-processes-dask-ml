543e50baa7fe58983b755fe2237212eb9cc30ab2
= <jonhur@web.de>
2025-09-18 10:31:29 +0200

    renamed ENVs

--- openeo_processes_dask_ml/process_implementations/constants.py
+++ openeo_processes_dask_ml/process_implementations/constants.py
@@ -1,13 +1,15 @@
 import os
 
-CACHE_DIR = os.environ.get("CACHE_DIR", "./cache")
-MODEL_CACHE_DIR = os.environ.get("MODEL_CACHE_DIR", f"{CACHE_DIR}/model_cache")
-DATACUBE_CACHE_DIR = os.environ.get("DATACUBE_CACHE_DIR", f"{CACHE_DIR}/datacubes")
+CACHE_DIR = os.environ.get("OPD_ML_CACHE_DIR", "./cache")
+MODEL_CACHE_DIR = os.environ.get("OPD_ML_MODEL_CACHE_DIR", f"{CACHE_DIR}/model_cache")
+DATACUBE_CACHE_DIR = os.environ.get(
+    "OPD_ML_DATACUBE_CACHE_DIR", f"{CACHE_DIR}/datacubes"
+)
 
-S3_MODEL_REPO_ENDPOINT = os.environ.get("S3_MODEL_REPO_ENDPOINT", None)
-S3_MODEL_REPO_ACCESS_KEY_ID = os.environ.get("S3_MODEL_REPO_ACCESS_KEY_ID", None)
+S3_MODEL_REPO_ENDPOINT = os.environ.get("OPD_ML_S3_MODEL_REPO_ENDPOINT", None)
+S3_MODEL_REPO_ACCESS_KEY_ID = os.environ.get("OPD_ML_S3_MODEL_REPO_ACCESS_KEY_ID", None)
 S3_MODEL_REPO_SECRET_ACCESS_KEY = os.environ.get(
-    "S3_MODEL_REPO_SECRET_ACCESS_KEY", None
+    "OPD_ML_S3_MODEL_REPO_SECRET_ACCESS_KEY", None
 )
 
 # make sure that both access_key_id and access_key are set
