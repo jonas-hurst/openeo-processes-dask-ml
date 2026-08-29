53f7511dda2a090f428182fa5f6e5a65e726b0fe
= <jonhur@web.de>
2026-08-29 15:51:20 +0200

    improve saving

--- openeo_processes_dask_ml/process_implementations/save_embeddings.py
+++ openeo_processes_dask_ml/process_implementations/save_embeddings.py
@@ -52,9 +52,7 @@ def _get_stac_item_template(_id: str) -> dict:
 
 
 def _save_as_zarr(datacube: xr.DataArray, result_dir: Path, zarr_dir: Path) -> Delayed:
-    saved = datacube.to_zarr(
-        zarr_dir, mode="w", zarr_format=3, consolidated=True, compute=False
-    )
+    saved = datacube.to_zarr(zarr_dir, compute=False)
     zip_path = delayed(zip_utils.create_zip_archive)(
         result_dir, zarr_dir, "results.zarr.zip", saved
     )
