08cac80a31b7556d2bc866c0352a9a32c80b8dfc
= <jonhur@web.de>
2026-08-29 15:52:07 +0200

    import load_embeddings function

--- openeo_processes_dask_ml/process_implementations/__init__.py
+++ openeo_processes_dask_ml/process_implementations/__init__.py
@@ -1,4 +1,5 @@
 from .init_models import mlm_class_random_forest
+from .load_embeddings import load_embeddings
 from .load_model import load_stac_ml
 from .ml_fit import ml_fit
 from .ml_predict import ml_predict
